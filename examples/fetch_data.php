10597d6187b6c62b7750bb5e6d23385ce83ebeb8
xZero707 <xzero@elite7hackers.net>
2018-12-11 23:45:48 +0100

    Add data example - /api/v2/data/link

---
+++ examples/fetch_data.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace NorthernLights\Client\Polr\Example;
+
+use NorthernLights\Client\Polr\ApiClient;
+use NorthernLights\Client\Polr\Attributes\DataAttributes;
+use NorthernLights\Client\Polr\Attributes\DataAttributesInterface;
+use NorthernLights\Client\Polr\Config\Config;
+use NorthernLights\Client\Polr\Response\DataResponseInterface;
+
+require __DIR__ . '/config.php';
+/** @var Config $config */
+
+/**
+ * Initialize API client and pass $config to it (DI)
+ * @var ApiClient $api
+ */
+$api = new ApiClient($config);
+
+$todayDate = date('Y-m-d H:i:s');
+
+/**
+ * Initialize attributes
+ * Possibility to supply additional parameters to API
+ *
+ * This step is optional
+ *
+ * @var DataAttributesInterface $attributes
+ */
+$attributes = new DataAttributes([
+    'left_bound' => $todayDate,
+    'right_bound' => $todayDate,
+    'stats_type' => 'day'
+]);
+
+/**
+ * Perform lookup. Argument $attributes, is optional
+ * @var DataResponseInterface $response
+ */
+$response = $api->data('polr', $attributes);
+
+// Check if API request was successful. Remains true as long as HTTP status code equals 200 OK
+if ($response->wasSuccessful()) {
+    echo 'There was an error querying the api' . PHP_EOL;
+}
+
+/***
+ * Dump results
+ */
+
+echo 'Response object: ' . PHP_EOL;
+dump($response);
+
+echo 'HTTP Response:' . PHP_EOL;
+dump(
+    $response->getHttpResponse()->getBody()->__toString()
+);
